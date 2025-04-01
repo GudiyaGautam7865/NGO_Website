54c29c63eee55abd95b59864140e56d7562ff793
unknown <GudiyaGautam7865>
2025-04-01 11:28:45 +0530

    first

--- src/pages/Donation.jsx
+++ src/pages/Donation.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import DonationHero from '../components/DonatePage/DonationHero'
 import NewsSection from '../components/DonatePage/NewsSection'
 import CardSection from '../components/home/CardSection'
-import Testimonials from "../components/DonatePage/Testimonials"; // Capitalized
+// import Testimonials from "../components/DonatePage/Testimonials"; // Capitalized
+import Test from '../components/DonatePage/Test'
 function Donation() {
   return (
     <div>
       <DonationHero/>
       <NewsSection/>
       <CardSection/>
-      <Testimonials/>
+      {/* <Testimonials/> */}
+      <Test />
       </div>
   )
 }
