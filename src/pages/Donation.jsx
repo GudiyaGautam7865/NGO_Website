1ef717626b148145b91aa61ed7428a34302d7311
unknown <GudiyaGautam7865>
2025-04-01 11:25:27 +0530

    first commie

--- src/pages/Donation.jsx
+++ src/pages/Donation.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import DonationHero from '../components/DonatePage/DonationHero'
 import NewsSection from '../components/DonatePage/NewsSection'
 import CardSection from '../components/home/CardSection'
-import Testimonials from '../components/DonatePage/Testimonials'
+import Testimonials from "../components/DonatePage/Testimonials"; // Capitalized
 function Donation() {
   return (
     <div>
