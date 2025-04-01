b7c0788c27ca853c7b5d5ae4ccbc211d77135e7e
unknown <GudiyaGautam7865>
2025-04-01 11:15:47 +0530

    first commitse

--- src/pages/Donation.jsx
+++ src/pages/Donation.jsx
@@ -1,25 +1,14 @@
 import React from 'react'
-
 import DonationHero from '../components/DonatePage/DonationHero'
-
-
-
-import NewsSection from '../components/DonatePage/NewsSection'
+// import NewsSection from '../components/DonatePage/NewsSection'
 import CardSection from '../components/home/CardSection'
-
-
 function Donation() {
   return (
     <div>
       <DonationHero/>
-      <NewsSection/>
+      {/* <NewsSection/> */}
       <CardSection/>
-
-   
-  
-    
-
-    </div>
+      </div>
   )
 }
 
